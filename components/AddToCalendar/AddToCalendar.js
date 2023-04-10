dd01cdb248053b9ad534a7567c995be87a6f03e4
FJ <jamifonline@gmail.com>
2023-04-09 22:54:56 -0500

    adding calendars component

--- components/AddToCalendar/AddToCalendar.js
+++ components/AddToCalendar/AddToCalendar.js
@@ -1,74 +1,86 @@
 import { useState } from "react";
 
-/* update calnedar details with your personal information */
 const AddToCalendar = () => {
   const [event, setEvent] = useState({
-    title: "Chat with FJ hiring meeting",
+    title: "Schedule a hiring meeting",
     description:
-      'Discuss background and upcoming hiring tasks.  email ▶️"faradeen@outlook.com" ',
-    /* you can import you image from a cdn like Linkedin or Github,  copy paste image url/link */
-    website:"https://fj360.dev",
+      "Discuss job details with candidate. Discuss background and qualifications. Applicant website: https://fj360.dev Applicant social: https://linkedin.com/in/faradeen",
     location: "Online",
-    startTime: "2023-03-15T10:00:00-04:00",
-    endTime: "2023-03-15T11:00:00-04:00",
+    startTime: "2023-04-10T04:15:00+00:00",
+    endTime: "2023-04-10T04:15:00+00:00",
   });
 
-  /* outlook */
+  const constructGoogleUrl = () => {
+    const googleCalendarBaseUrl =
+      "https://calendar.google.com/calendar/render?action=TEMPLATE";
+    const googleCalendarParams = new URLSearchParams({
+      text: event.title,
+      details: event.description,
+      location: event.location,
+      dates: `${new Date(event.startTime).toISOString()}/${new Date(
+        event.endTime
+      ).toISOString()}`,
+    });
+    return `${googleCalendarBaseUrl}&${googleCalendarParams.toString()}`;
+  };
+
+  const constructOutlookLiveUrl = () => {
+    const outlookLiveCalendarBaseUrl =
+      "https://outlook.live.com/calendar/0/deeplink/compose";
+    const outlookLiveCalendarParams = new URLSearchParams({
+      subject: event.title,
+      body: event.description,
+      location: event.location,
+      startdt: new Date(event.startTime).toISOString(),
+      enddt: new Date(event.endTime).toISOString(),
+      path: "/calendar/action/compose",
+      rru: "addevent",
+    });
+    return `${outlookLiveCalendarBaseUrl}?${outlookLiveCalendarParams.toString()}`;
+  };
+
   const constructOutlookUrl = () => {
     const outlookCalendarBaseUrl =
       "https://outlook.office.com/calendar/0/deeplink/compose";
     const outlookCalendarParams = new URLSearchParams({
       subject: event.title,
+      body: event.description,
       location: event.location,
-      body: event.description + event.website,
       startdt: new Date(event.startTime).toISOString(),
       enddt: new Date(event.endTime).toISOString(),
-      allday: false,
+      path: "/calendar/action/compose",
+      rru: "addevent",
     });
     return `${outlookCalendarBaseUrl}?${outlookCalendarParams.toString()}`;
   };
-
-  const constructAppleUrl = () => {
-    const calendarData = `BEGIN:VCALENDAR
-  PRODID:-//My Company//EN
-  VERSION:2.0
-  CALSCALE:GREGORIAN
-  BEGIN:VEVENT
-  UID:${event.id}
-  DTSTART:${new Date(event.startTime).toISOString().replace(/[:-]/g, "")}
-  DTEND:${new Date(event.endTime).toISOString().replace(/[:-]/g, "")}
-  SUMMARY:${event.title}
-  LOCATION:${event.location}
-  DESCRIPTION:${event.description}
-  END:VEVENT
-  END:VCALENDAR`;
-  
-    return URL.createObjectURL(new Blob([calendarData], { type: 'text/calendar' }));
-  };
-  
 
   const openCalendar = (url) => {
     window.open(url, "_blank");
   };
 
   return (
-    <div className="calendar-footer flex justify-center   mt-0">
-      <div className="btn-gradient">
-        <button
-          className="outlook-btn btn  hover:bg-white hover:text-black opacity-95 hover:opacity-100 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          onClick={() => openCalendar(constructOutlookUrl())}
-        >
-          <span className="add-to-cal-plus">+</span> Outlook Calendar
-        </button>
-      </div>
-      <button
-        className="apple-btn  hover:bg-white hover:text-black opacity-95 hover:opacity-100 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        onClick={() => openCalendar(constructAppleUrl())}
-      >
-        <span className="add-to-cal-plus">+</span> Apple Calendar
+    <><div className="calendar-bts-box calendar-footer flex justify-center   mt-0">
+      <button className="calendar-btns  hover:bg-white hover:text-black font- opacity-95 hover:opacity-100 py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+       onClick={() => openCalendar(constructGoogleUrl())}>
+        Google Calendar
       </button>
-    </div>
+      <button className="calendar-btns hover:bg-white hover:text-black font- opacity-95 hover:opacity-100 py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+       onClick={() => openCalendar(constructOutlookLiveUrl())}>
+        365 Live Calendar
+      </button>
+      <button className="calendar-btns hover:bg-white hover:text-black font- opacity-95 hover:opacity-100 py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
+       onClick={() => openCalendar(constructOutlookUrl())}>
+        Outlook Calendar
+      </button>
+    </div></>
+   
+  
   );
 };
 
+
 export default AddToCalendar;
+
+
+
+
